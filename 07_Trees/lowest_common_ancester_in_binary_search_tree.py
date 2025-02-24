5c7d8ecc81c998d820211bd6fa31b8bcca311f75
Cs944612 <shecha273@gmail.com>
2025-02-24 06:16:39 +0530

    Solved 'Lowest Common Ancestor in Binary Search Tree' Trees Blind-75

---
+++ 07_Trees/lowest_common_ancester_in_binary_search_tree.py
@@ -0,0 +1,48 @@
+"""
+Lowest Common Ancestor in Binary Search Tree
+Problem: LeetCode 235 - Lowest Common Ancestor of a Binary Search Tree
+Difficulty: Medium
+
+Given a binary search tree (BST) where all node values are unique, and two nodes from the tree p and q, return the lowest common ancestor (LCA) of the two nodes.
+
+The lowest common ancestor between two nodes p and q is the lowest node in a tree T such that both p and q as descendants. The ancestor is allowed to be a descendant of itself.
+
+Example 1:
+Input: root = [5,3,8,1,4,7,9,null,2], p = 3, q = 8
+Output: 5
+
+Example 2:
+Input: root = [5,3,8,1,4,7,9,null,2], p = 3, q = 4
+Output: 3
+Explanation: The LCA of nodes 3 and 4 is 3, since a node can be a descendant of itself.
+
+Constraints:
+2 <= The number of nodes in the tree <= 100.
+-100 <= Node.val <= 100
+p != q
+p and q will both exist in the BST.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        # curr = root     # current
+        # while curr:
+            # if p and q are greater than the current node, move to the right
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            # if p and q are less than the current node, move to the left
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+
+            else:
+                return curr
+        # If the loop ends, return None (though the problem guarantees p and q exist in the BST)
+        return None
+ 
